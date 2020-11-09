11878859ac71107610db381993c38ef5b3ca91e5
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2020-11-09 11:48:09 -0700

    Create index action

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,4 +1,7 @@
 class PatientsController < ApplicationController
+  def index
+    @patients = Patient.sorted_by_age
+  end
   def destroy
     doctor = Doctor.find(params[:doctor_id])
     patient = doctor.find_patient(params[:id])
