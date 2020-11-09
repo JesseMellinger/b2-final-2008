e0a77bb6a358026ef7430b24a7791cea17a09ae0
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2020-11-09 11:47:17 -0700

    Add route for patient index page

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   resources :doctors, only: :show do
     resources :patients, only: :destroy
   end
+
   resources :hospitals, only: :show
+
+  resources :patients, only: :index
 end
