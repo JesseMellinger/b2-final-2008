06c3dcc9d54a96b15d1228b1b9554e43dad3d33a
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2020-11-09 11:29:31 -0700

    Create instance method to find doctor patient record

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -2,4 +2,8 @@ class Doctor < ApplicationRecord
   belongs_to :hospital
   has_many :doctor_patients
   has_many :patients, through: :doctor_patients
+
+  def find_patient(patient_id)
+    doctor_patients.find(patient_id)
+  end
 end
