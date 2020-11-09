f188fd7ea25c9bc40b20ff05a0dd9c91e8402041
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2020-11-09 10:43:29 -0700

    Create instance methods

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -1,3 +1,12 @@
 class Hospital < ApplicationRecord
   has_many :doctors
+
+  def number_of_doctors
+    doctors.count
+  end
+
+  def unique_universities
+    doctors.distinct
+           .pluck(:university)
+  end
 end
