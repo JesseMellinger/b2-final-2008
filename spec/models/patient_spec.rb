8d81a20fd32d89388195a1253006f5b452b832fc
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2020-11-09 11:46:18 -0700

    Create test for class method

--- spec/models/patient_spec.rb
+++ spec/models/patient_spec.rb
@@ -5,4 +5,18 @@ RSpec.describe Patient do
     it {should have_many :doctor_patients}
     it {should have_many(:doctors).through(:doctor_patients)}
   end
+
+  describe 'class methods' do
+    before :each do
+      @patient_1 = Patient.create!(name: "Katie Bryce", age: 24)
+      @patient_2 = Patient.create!(name: "Denny Duquette", age: 39)
+      @patient_3 = Patient.create!(name: "Rebecca Pope", age: 32)
+      @patient_4 = Patient.create!(name: "Zola Shepherd", age: 2)
+    end
+
+    it '.sorted_by_age' do
+      expected = [@patient_2, @patient_3, @patient_1, @patient_4]
+      expect(Patient.sorted_by_age).to eq(expected)
+    end
+  end
 end
